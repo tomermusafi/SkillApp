790dd7b56d1224643d490094ca5e837fdf600dc2
Eliko <yyyeliko@gmail.com>
2020-04-30 18:52:43 +0300

    Added Classes

---
+++ app/src/androidTest/java/info/Person.java
@@ -0,0 +1,16 @@
+package info;
+
+public class Person {
+	String id;
+	String name;
+	String email;
+	Lesson lessons_took[];
+	Lesson lessons_make[];
+	Category follow_categories[];
+	SubCategory follow_subcategories[];
+	int currentAmount;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+}
