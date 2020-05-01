a0887be0e5d1f5076ed9accb3f8d8c311d8db87e
Eliko <yyyeliko@gmail.com>
2020-05-01 04:53:38 +0300

    Coupons fix

--- app/src/main/java/com/musafi/skillapp/info/Main.java
+++ app/src/main/java/com/musafi/skillapp/info/Main.java
@@ -13,7 +13,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Start..");
-//		/* Print Categories */
+		/* Print Categories */
+		
 //		List<Category> categories = getAllCategories();
 //		for (int i = 0; i < categories.size(); i++) {
 //			System.out.println("Category: " + categories.get(i));
@@ -28,7 +29,6 @@ public class Main {
 //		}
 //		System.out.println(subCategoriesNames);
 
-//		Person jacob = new Person("Jacob", "jacob123@gmail.com", 5);
 		Category category1 = new Category("Board Games");
 		SubCategory subcategory1 = new SubCategory("Chess", category1);
 //		category1.addSubCategory(subcategory1);
@@ -156,10 +156,10 @@ public class Main {
 	public static List<Coupon> getAllCoupons() {
 		List<Coupon> coupons = new ArrayList<Coupon>();
 		
-		coupons.add(new Coupon("25% off on Adidas Clothing", "CafeCafe", 20, "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/39a7f46af8524e0fa095a95901199987_9366/tiro-19-training-pants.jpg"));
+		coupons.add(new Coupon("25% off on Adidas Clothing", "Adidas", 20, "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/39a7f46af8524e0fa095a95901199987_9366/tiro-19-training-pants.jpg"));
 		coupons.add(new Coupon("10% off on Pizza", "Dominos", 7, "https://besttv232-ynet-images1-prod.cdn.it.best-tv.com/PicServer5/2019/11/23/9613160/big.jpg"));
 		coupons.add(new Coupon("7% off on Dell Laptop", "Dell", 20, "https://www.morlevi.co.il/cache/w_600,h_500,mode_pad/notebook-vostro-15-3580-3581-apj-emea-pdp-1(1).jpg"));
-		coupons.add(new Coupon("15% off on Ink", "Office Depot", 5, "https://www.mymzone.com/wp-content/uploads/2019/09/Digital-Slider-300x200.jpg"));
+		coupons.add(new Coupon("15% off on TV 4K", "Samsung", 5, "https://www.google.com/search?q=SAMSUNG+TV+4K&sxsrf=ALeKk014lxrcBByTGD87HuPmlq9ey6xaaA:1588297514433&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj4x5SpxZHpAhVPLewKHdreDAcQ_AUoAXoECAwQAw&biw=1680&bih=907#imgrc=chylBo7hqerxEM"));
 		coupons.add(new Coupon("20% off on Washing Machine", "Michele", 50, "https://miro.medium.com/max/1400/1*LidfgXsTgTYjzb6AY9cMFw.jpeg"));
 		coupons.add(new Coupon("17% off on any Udemy Course", "Udemy", 10, "https://230027-986802-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/04/Are-Udemy-courses-worth-putting-on-resume.png"));
 		coupons.add(new Coupon("80% off on Breakfast for Couple", "CafeCafe", 20, "https://pbs.twimg.com/media/DwNhJzEX0AENUn5?format=jpg&name=small"));
